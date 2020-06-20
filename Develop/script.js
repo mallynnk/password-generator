b9b6ce5186d361083a6af151c38364fc4db097e3
mallynnk <mallory.korpics@gmail.com>
2020-06-20 18:47:00 -0400

    renamed variables within functions and added returns

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,18 @@
 
 var lengthReq = function() {
-    while (true) {
-      length = window.prompt("Please enter a password length between 8 and 128 characters");
+    length = window.prompt("Please enter a password length between 8 and 128 characters");
       console.log(length)
       if (length >= 8 && length <=128){
         return length;
       }
       else {
         window.alert("please enter a valid option");
+      lengthReq();     
       }
-      lengthReq();
-      break;
-    }
-};
+    };
 
-var uppercaseChoice = function() {
+
+var getUppercase = function() {
   var uppercase = window.prompt("Would you like uppercase letters in your password, yes or no?") 
   uppercase = uppercase.toLowerCase(); 
   if (uppercase === "yes") {
@@ -25,11 +23,12 @@ var uppercaseChoice = function() {
   }
   else {
     window.alert("please enter a valid option!");
-   return this.uppercaseChoice()
+   return this.getUppercase()
   }
+  return uppercaseChoice
 };
 
-var lowercaseChoice = function() {
+var getLowercase = function() {
   var lowercase = window.prompt("Would you like lowercase letters in your password, yes or no?") 
   lowercase = lowercase.toLowerCase(); 
   if (lowercase === "yes") {
@@ -40,11 +39,12 @@ var lowercaseChoice = function() {
   }
   else {
     window.alert("please enter a valid option!");
-   return this.lowercaseChoice()
+   return this.getLowercase()
   }
+  return lowercaseChoice;
 };
 
-var numbersChoice = function() {
+var getNumbers = function() {
   var numbers = window.prompt("Would you like numbers letters in your password, yes or no?") 
   numbers = numbers.toLowerCase(); 
   if (numbers === "yes") {
@@ -55,11 +55,12 @@ var numbersChoice = function() {
   }
   else {
     window.alert("please enter a valid option!");
-   return this.numbersChoice()
+   return this.getNumbers()
   }
+  return numbersChoice;
 };
 
-var specialCharChoice = function() {
+var getSpecialChar = function() {
   var specialChar = window.prompt("Would you like numbers letters in your password, yes or no?") 
   specialChar = specialChar.toLowerCase(); 
   if (specialChar === "yes") {
@@ -70,73 +71,11 @@ var specialCharChoice = function() {
   }
   else {
     window.alert("please enter a valid option!");
-   return this.specialCharChoice()
+   return this.getSpecialChar()
   }
+  return specialCharChoice;
 };
 
-
-var generate = function () {
- //add all variables together
- var allChoices = uppercaseChoice + lowercaseChoice + numbersChoice + specialCharChoice 
- //set password blank to start
- var password = "";
- //for loop to add values till length stops the loop 
-         //reference: https://randompasswordgenerator.org/javascript-password-generator.html 
- for (var i = 0; i < length; i++) {
-     var character = Math.floor(Math.random() * allChoices.length);
-     password += allChoices.substring(character, character + 1);
- }
- //return the value of all values from the for loop
- return password;
-};
-
-
-
-// var generate = function() {
-//   var length = lengthReq();
-  
-//   // ask what they would like in their password
-
-
-// var lowercase = window.prompt("Would you like lowercase letters in your password, yes or no?")
-// lowercase = lowercase.toLowerCase();
-// if (lowercase === "yes") { 
-//   var lowercase = "abcdefghijklmnopqrstuvwxyz";
-// } 
-// else {
-//   lowercase = "";
-// }
-// console.log(lowercase);
-
-// var numbers = window.prompt("Would you like numbers in your password, yes or no?")
-// numbers = numbers.toLowerCase();
-// if (numbers === "yes") {
-//   var numbers = "0123456789";
-// } 
-// else {
-//   numbers = "";
-// }
-// console.log(numbers);
-
-// var specialChar = window.prompt("Would you like special characters in your password, yes or no?")
-// specialChar = specialChar.toLowerCase();
-// if (specialChar === "yes") {
-//   var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-// } 
-// else {
-//   specialChar = ""
-// }
-// console.log(specialChar);
-// }
-
-// function startGenerator() {
-//   if (generateBtn) {
-//     lengthReq();
-//   }
-// };
-
-
-// generate();
 
 
 
