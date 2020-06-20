0c4af591b44787927b349e05b51c2336a2435e3a
mallynnk <mallory.korpics@gmail.com>
2020-06-20 13:09:35 -0400

    added length requirement and password requirement functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,145 @@
-// Assignment code here
 
+var lengthReq = function() {
+    while (true) {
+      length = window.prompt("Please enter a password length between 8 and 128 characters");
+      console.log(length)
+      if (length >= 8 && length <=128){
+        return length;
+      }
+      else {
+        window.alert("please enter a valid option");
+      }
+      lengthReq();
+      break;
+    }
+};
+
+var uppercaseChoice = function() {
+  var uppercase = window.prompt("Would you like uppercase letters in your password, yes or no?") 
+  uppercase = uppercase.toLowerCase(); 
+  if (uppercase === "yes") {
+    var uppercaseChoice = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+  } 
+  else if (uppercase === "no") {
+    var uppercaseChoice = ""
+  }
+  else {
+    window.alert("please enter a valid option!");
+   return this.uppercaseChoice()
+  }
+};
+
+var lowercaseChoice = function() {
+  var lowercase = window.prompt("Would you like lowercase letters in your password, yes or no?") 
+  lowercase = lowercase.toLowerCase(); 
+  if (lowercase === "yes") {
+    var lowercaseChoice = "abcdefghijklmnopqrstuvwxyz" 
+  } 
+  else if (lowercase === "no") {
+    var lowercaseChoice = ""
+  }
+  else {
+    window.alert("please enter a valid option!");
+   return this.lowercaseChoice()
+  }
+};
+
+var numbersChoice = function() {
+  var numbers = window.prompt("Would you like numbers letters in your password, yes or no?") 
+  numbers = numbers.toLowerCase(); 
+  if (numbers === "yes") {
+    var numbersChoice = "0123456789" 
+  } 
+  else if (numbers === "no") {
+    var numbersChoice = ""
+  }
+  else {
+    window.alert("please enter a valid option!");
+   return this.numbersChoice()
+  }
+};
+
+var specialCharChoice = function() {
+  var specialChar = window.prompt("Would you like numbers letters in your password, yes or no?") 
+  specialChar = specialChar.toLowerCase(); 
+  if (specialChar === "yes") {
+    var specialCharChoice = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } 
+  else if (specialChar === "no") {
+    var specialCharChoice = ""
+  }
+  else {
+    window.alert("please enter a valid option!");
+   return this.specialCharChoice()
+  }
+};
+
+
+
+
+// var generate = function() {
+//   var length = lengthReq();
+  
+//   // ask what they would like in their password
+
+
+// var lowercase = window.prompt("Would you like lowercase letters in your password, yes or no?")
+// lowercase = lowercase.toLowerCase();
+// if (lowercase === "yes") { 
+//   var lowercase = "abcdefghijklmnopqrstuvwxyz";
+// } 
+// else {
+//   lowercase = "";
+// }
+// console.log(lowercase);
+
+// var numbers = window.prompt("Would you like numbers in your password, yes or no?")
+// numbers = numbers.toLowerCase();
+// if (numbers === "yes") {
+//   var numbers = "0123456789";
+// } 
+// else {
+//   numbers = "";
+// }
+// console.log(numbers);
+
+// var specialChar = window.prompt("Would you like special characters in your password, yes or no?")
+// specialChar = specialChar.toLowerCase();
+// if (specialChar === "yes") {
+//   var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+// } 
+// else {
+//   specialChar = ""
+// }
+// console.log(specialChar);
+// }
+
+// function startGenerator() {
+//   if (generateBtn) {
+//     lengthReq();
+//   }
+// };
+
+
+// generate();
+
+
+
+
+
+// variables
+
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+var password = "";
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
+var generateBtn = document.querySelector("#generate");
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
